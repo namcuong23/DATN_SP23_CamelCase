2b62dc04738f50f5cda6a2d88332b1d9f10d621f
leanhthai132002 <vanuong132002@gmail.com>
2023-11-15 11:41:32 +0700

    fix chart

--- src/components/Admin/home/ChartLine/StatisticalPackagePie.tsx
+++ src/components/Admin/home/ChartLine/StatisticalPackagePie.tsx
@@ -1,64 +1,70 @@
+// @ts-nocheck
 import React from 'react';
-import { Line } from '@ant-design/plots';
+import { Pie } from '@ant-design/plots';
 import { Order } from '../../../../interface/admin/order';
-
+import { useGetUsersQuery } from '../../../../service/auth';
+import { useGetUsersEprQuery } from '../../../../service/auth_employer';
 interface ChildComponentProps {
-  PackageHistory: Order[] | undefined;
+  PackageHistory: Order[];
 }
+interface ChartData {
+  [x: string]: any;
+  Date: string;
+  scales: string,
+}
+const StatisticalPackagePie : React.FC<ChildComponentProps> = ({ PackageHistory }) =>{
 
-const RevenueLineChartDay: React.FC<ChildComponentProps> = ({ PackageHistory }) => {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1; // Lấy tháng hiện tại
-  const daysInMonth = new Date(currentYear, currentMonth, 0).getDate(); // Lấy số ngày trong tháng hiện tại
+  const { data: userEpe } = useGetUsersQuery();
+  const { data: userEpr } = useGetUsersEprQuery('');
+  
+  const data = [
+    {
+      type: 'Company',
+      value: userEpe?.length,
+    },
+    {
+      type: 'User',
+      value: userEpr?.length,
 
-  const daysInMonthArray = Array.from({ length: daysInMonth }, (_, index) => index + 1);
-
-  const revenueData = daysInMonthArray.map((day) => {
-    if (PackageHistory) {
-      const ordersInDay = PackageHistory.filter((order) => {
-        const orderDate = new Date(order.createdAt);
-        return (
-          orderDate.getFullYear() === currentYear &&
-          orderDate.getMonth() + 1 === currentMonth &&
-          orderDate.getDate() === day
-        );
-      });
-
-      const totalRevenue = ordersInDay.reduce((total, order) => total + order.order_price, 0);
-
-      return {
-        day: day,
-        revenue: totalRevenue,
-      };
-    }
-
-    return null;
-  }).filter(Boolean);
-
+    },
+  ];
   const config = {
-    data: revenueData,
-    xField: 'day',
-    yField: 'revenue',
-    xAxis: {
-      type: 'cat',
-    },
-    yAxis: {
-      type: 'linear',
-      min: 0,
-      nice: true,
-    },
-    smooth: true,
+    appendPadding: 10,
+    data,
+    angleField: 'value',
+    colorField: 'type',
+    radius: 1,
+    innerRadius: 0.6,
     label: {
-      position: 'middle',
+      type: 'inner',
+      offset: '-50%',
+      content: '{value}',
       style: {
-        fill: '#FFFFFF',
-        opacity: 0.6,
+        textAlign: 'center',
+        fontSize: 14,
+      },
+    },
+    interactions: [
+      {
+        type: 'element-selected',
+      },
+      {
+        type: 'element-active',
+      },
+    ],
+    statistic: {
+      title: false,
+      content: {
+        style: {
+          whiteSpace: 'pre-wrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        },
+        content: '',
       },
     },
   };
-
-  return <Line {...config} />;
+  return <Pie {...config} />;
 };
 
-export default RevenueLineChartDay;
+export default StatisticalPackagePie
